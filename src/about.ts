07e31bfb7af2292f59f9216c6dc55dd5f247f19c
Emma Bastås <emma.bastas@protonmail.com>
2025-07-04 22:40:15 +0200

    Write some more in the about

--- src/about.ts
+++ src/about.ts
@@ -1,8 +1,7 @@
+//@ts-ignore
+import html from "bundle-text:./about.html"
+
 export function takeover() {
-  document.body.innerHTML = `
-  <m-col class="w-full h-full gap-4 text-xl justify-center items-center bg-gray-200">
-    This is the about page. I will write something here at some point
-    <m-route href="/">Take me back home!</m-route>
-  </m-col>
-  `
+  document.title = "pdf-template"
+  document.body.innerHTML = html
 }
